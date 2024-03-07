e0ac8f915adf16d194c99a11f35ebc98bb0e03bc
fengnn <fengnn@kmelearning.com>
2024-03-07 16:26:29 +0800

    ci: 集成 shadcn-ui

--- app/issue/page.tsx
+++ app/issue/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const IssuePage = () => {
-  return <div className="p-6">IssuePage</div>;
+  return (
+    <div className="p-6">
+      <Button variant="default">New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuePage;
