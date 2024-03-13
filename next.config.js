8324abdaabf4ed2151d17080528deaf2c41fb5cb
fengnn <fengnn@kmelearning.com>
2024-03-13 13:38:18 +0800

    feat:  解决google 头像不显示的问题

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "referrerPolicy",
+            value: "no-referrer",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
